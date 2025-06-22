a6ef4fb0d87d57fd9ea44f40b6916e2f9807b614
AhtishamAhmed <Ahtisham1322@gmail.com>
2025-06-22 22:51:25 +0500

    created product endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category,
+      string Description, string Imagefile, decimal Price)
+        :IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create a product
+            //save to database
+            //return value
+            var product = new Product{
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.Imagefile,
+                Price = command.Price,
+            };
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+}
